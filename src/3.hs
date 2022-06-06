b42550f7db12a03d9a57ae93580a03b4bf2f02b1
Michiel Peeters <michielpeeters7@gmail.com>
2022-06-06 15:02:54 +0200

    remove unecessary let

--- src/3.hs
+++ src/3.hs
@@ -3,7 +3,7 @@ find f [] = Nothing
 find f (x:xs) = if f x then Just x else find f xs
 
 primeFactors :: Int -> [Int]
-primeFactors n = let result = getSmallestDivisor in case result of
+primeFactors n = case getSmallestDivisor of
     Nothing -> [n]
     Just x -> x : primeFactors (div n x)
     where 
