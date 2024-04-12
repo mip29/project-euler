4a8940b871fc22d55cfcba2f4177966284921dbb
Michiel Peeters <michielpeeters7@gmail.com>
2024-04-13 00:11:44 +0200

    rework problem 1

--- src/1.hs
+++ src/1.hs
@@ -1,8 +1,1 @@
-isMultipleOf3Or5 :: Int -> Bool
-isMultipleOf3Or5 n = mod n 3 == 0 || mod n 5 == 0
-
-sumBelow :: Int -> Int
-sumBelow n = sum $ filter isMultipleOf3Or5 [0..n-1]
-
-solution :: Int
-solution = sumBelow 1000
+solution = sum $ filter (\n -> n `mod` 3 == 0 || n `mod` 5 == 0) [0 .. 999]
