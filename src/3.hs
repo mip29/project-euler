ceb60585831a94d89e024db1d696f5badd4d1702
Michiel Peeters <michielpeeters7@gmail.com>
2024-04-13 23:24:15 +0200

    refactor problem 3

--- src/3.hs
+++ src/3.hs
@@ -1,13 +1,3 @@
-find :: (a -> Bool) -> [a] -> Maybe a
-find f [] = Nothing
-find f (x : xs) = if f x then Just x else find f xs
+import Math.Util (factors)
 
-primeFactors :: Int -> [Int]
-primeFactors n = case getSmallestDivisor of
-  Nothing -> [n]
-  Just x -> x : primeFactors (div n x)
-  where
-    getSmallestDivisor = find (\x -> n `mod` x == 0) [2 .. n - 1]
-
-solution :: Int
-solution = last (primeFactors 600851475143)
+solution = last (factors 600851475143)
--- src/Math/Util.hs
+++ src/Math/Util.hs
@@ -1,7 +1,33 @@
-module Math.Util where
+module Math.Util
+  ( fibonacci,
+    factors,
+    primes,
+    lcmm,
+  )
+where
 
 fibonacci :: [Integer]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
+
+-- only works on ordered lists
+minus :: (Ord a) => [a] -> [a] -> [a]
+minus (x : xs) (y : ys) = case compare x y of
+  LT -> x : minus xs (y : ys)
+  EQ -> minus xs ys
+  GT -> minus (x : xs) ys
+minus xs _ = xs
+
+factors :: Integer -> [Integer]
+factors n = f n (head primes) (tail primes)
+  where
+    f n p ps
+      | n < 2 = []
+      | n < p ^ 2 = [n]
+      | n `mod` p == 0 = p : f (n `div` p) p ps
+      | otherwise = f n (head ps) (tail ps)
+
+primes :: [Integer]
+primes = 2 : filter (\n -> head (factors n) == n) [3, 5 ..]
 
 lcmm :: (Integral a, Foldable t) => t a -> a
 lcmm = foldr lcm 1
