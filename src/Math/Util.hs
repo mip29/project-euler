c47499e07299b599f0c52857cc19650658985a02
Michiel Peeters <michielpeeters7@gmail.com>
2024-04-13 23:25:09 +0200

    removes unused function

--- src/Math/Util.hs
+++ src/Math/Util.hs
@@ -9,14 +9,6 @@ where
 fibonacci :: [Integer]
 fibonacci = 0 : 1 : zipWith (+) fibonacci (tail fibonacci)
 
--- only works on ordered lists
-minus :: (Ord a) => [a] -> [a] -> [a]
-minus (x : xs) (y : ys) = case compare x y of
-  LT -> x : minus xs (y : ys)
-  EQ -> minus xs ys
-  GT -> minus (x : xs) ys
-minus xs _ = xs
-
 factors :: Integer -> [Integer]
 factors n = f n (head primes) (tail primes)
   where
